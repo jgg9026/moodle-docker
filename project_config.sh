1ecde5d916057ce29a20e16d696880b77d3898b2
Jonathan Garcia <jgg.906@gmail.com>
2022-11-04 23:56:14 -0500

    Docker improvements and rancher support

---
+++ project_config.sh
@@ -0,0 +1,6 @@
+# Add your env settings here. Any other from the README.md file can be added here.
+export MOODLE_DOCKER_WWWROOT=/Users/jonathan.garcia/Documents/core-moodle
+export MOODLE_DOCKER_DB=mysql
+export MOODLE_DOCKER_DBNAME=m4.0
+export MOODLE_DOCKER_WEB_HOST=moodle.test
+export MOODLE_DOCKER_WEB_PORT=80
